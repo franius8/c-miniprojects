530ea3e751ae7d11e98ad007597e2b3f636170d0
franius8 <skawinskifranciszek@gmail.com>
2022-10-24 08:36:06 +0200

    Amend simple variable exercise

--- DataTypesAndVariables/simplevariableexercise.c
+++ DataTypesAndVariables/simplevariableexercise.c
@@ -8,4 +8,8 @@ int main() {
   printf("What is the greatest number? %u, of course.\n", number);
   char animal[3] = "cat";
   printf("What animal should I buy? The best option would be a %s.\n", animal);
+  float temperature = 14.4;
+  printf("The current temperature is %g.\n", temperature);
+  char letterB = 'B';
+  printf("The second letter of the alphabet is %c.\n", letterB);
 };
