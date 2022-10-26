3d437aafc928d7b378443edc9812c1a9839efab0
franius8 <skawinskifranciszek@gmail.com>
2022-10-26 21:50:43 +0200

    Add function collecting moves

--- tictactoe/tictactoe.c
+++ tictactoe/tictactoe.c
@@ -1,27 +1,38 @@
 #include <stdio.h>
 
 void printBoard ( void );
-void printRow ( void );
+void printRow ( int i );
 void printDivider ( void );
+int collectNextMove ( void );
 
 int main ( void ) {
   printBoard();
+  int field = collectNextMove();
   return 0;
 };
 
 void printBoard ( void ) {
   printDivider();
   for (int i = 0; i < 3; i++) {
-    printRow();
+    printRow(i);
     printDivider();
   };
 };
 
-void printRow ( void ) {
-  char x = 'x';
-  printf("| %c | %c | %c |\n", x, x, x);
+void printRow ( int i ) {
+  int one = 1 + (i * 3);
+  int two = 2 + (i * 3);
+  int three = 3 + (i * 3);
+  printf("| %d | %d | %d |\n", one, two, three);
 };
 
 void printDivider ( void ) {
   printf("-------------\n");
+}
+
+int collectNextMove ( void ) {
+  int num;
+  printf("Enter a number: ");
+  scanf("%d", &num);
+  return num;
 }
