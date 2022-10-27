d0242f2602ea79d401e69a95103185a07a6ec2a8
franius8 <skawinskifranciszek@gmail.com>
2022-10-27 09:01:17 +0200

    Create basic functions allowing to play the game

--- tictactoe/tictactoe.c
+++ tictactoe/tictactoe.c
@@ -2,17 +2,17 @@
 
 void printWelcomeMessage ( void );
 void playGame ( void );
+int isGameWon ( char board[10], char marker );
 void printBoard ( char gameBoard[10] );
 void printRow ( int i, char board[10] );
 void printDivider ( void );
-int collectNextMove ( void );
+int collectNextMove ( );
 int determinePrintCharacter( char board[10], int i, int column);
 
 
 int main ( void ) {
   printWelcomeMessage();
   playGame();
-  int field = collectNextMove();
   return 0;
 };
 
@@ -25,7 +25,56 @@ void printWelcomeMessage ( void ) {
 
 void playGame ( void ) {
   char board[10] = {'-', '-', '-', '-', '-', '-', '-', '-', '-', '-'};
-  printBoard(board);
+  int choice = 0;
+  int round = 0;
+  char currentPlayerMarker = 'x';
+  while (1) {
+    printBoard(board);
+    printf("Turn of the %c player.\n", currentPlayerMarker);
+    while (1) {
+      choice = collectNextMove();
+      if (board[choice] == '-') {
+        break;
+      }
+      printf("Invalid choice. Try again.\n");
+    }
+    board[choice] = currentPlayerMarker;
+    round++;
+    if (isGameWon( board, currentPlayerMarker ) == 1) {
+      printf("The %c player won!\n", currentPlayerMarker);
+      break;
+    }
+    if (round == 9) {
+      printf("Tie!\n");
+      break;
+    }
+    currentPlayerMarker = (currentPlayerMarker == 'x') ? 'o' : 'x';
+  }  
+}
+
+int isGameWon ( char board[10], char marker ) {
+  int winningCombinations[8][3] = {
+      {1, 2, 3}, 
+      {4, 5, 6}, 
+      {7, 8, 9}, 
+      {1, 4, 7}, 
+      {2, 5, 8}, 
+      {3, 6, 9}, 
+      {1, 5, 9}, 
+      {3, 5, 7}
+    };
+  for (int i = 0; i < 8; i++) {
+    int numberOfMarkers = 0;
+    for(int j = 0; j < 3; j++) {
+      if (board[winningCombinations[i][j]] == marker) {
+        numberOfMarkers++;
+      }
+    }
+    if (numberOfMarkers == 3) {
+      return 1;
+    }
+  }
+  return 0;
 }
 
 void printBoard ( char gameBoard[10] ) {
@@ -40,7 +89,6 @@ void printRow ( int i, char board[10] ) {
   int one = determinePrintCharacter(board, i, 1);
   int two = determinePrintCharacter(board, i, 2);
   int three = determinePrintCharacter(board, i, 3);
-  char four = 1 + (i * 3) + 48;
   printf("| %c | %c | %c |\n", one, two, three);
 };
 
@@ -48,21 +96,21 @@ void printDivider ( void ) {
   printf("-------------\n");
 }
 
-int collectNextMove ( void ) {
+int collectNextMove () {
   int num;
-  printf("Select the field where you want to place your marker: ");
-  scanf("%d", &num);
-  return num;
+    printf("Select the field where you want to place your marker: ");
+    scanf("%d", &num);
+    return num;
 }
 
 int determinePrintCharacter( char board[10], int i, int column) {
   char resultChar;
   char field = board[column + (i * 3)];
   if (field == '-') {
-    resultChar = 1 + (i * 3) + 48;
+    resultChar = column + (i * 3) + 48;
   } 
   else {
     resultChar = field;
   }
-  return field;
+  return resultChar;
 }
